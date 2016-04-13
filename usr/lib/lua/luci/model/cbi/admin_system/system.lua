5207d7a893230a87ba2ef5ab28830a9aadfd4ae2
Dave A-W <DaveA-W@users.noreply.github.com>
2016-04-13 20:46:01 +0800

    Enable selection of installed LuCI themes
    Added "Design" dropdown to the Language & Style tab on the System:System
    page.  Lists installed LuCI themes available for selection.

--- usr/lib/lua/luci/model/cbi/admin_system/system.lua
+++ usr/lib/lua/luci/model/cbi/admin_system/system.lua
@@ -126,4 +126,20 @@ function o.write(self, section, value)
 	m.uci:set("luci", "main", "lang", value)
 end
 
+
+o = s:taboption("language", ListValue, "_mediaurlbase", translate("Design"))
+for k, v in pairs(conf.themes) do
+	if k:sub(1, 1) ~= "." then
+		o:value(v, k)
+	end
+end
+
+function o.cfgvalue(...)
+	return m.uci:get("luci", "main", "mediaurlbase")
+end
+
+function o.write(self, section, value)
+	m.uci:set("luci", "main", "mediaurlbase", value)
+end
+
 return m
