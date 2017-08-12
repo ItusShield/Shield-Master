6d9304be80d5f38496d2c9bc1e8bd64e91480bf9
Dave A-W <dave.aw+git@gmail.com>
2017-08-12 16:13:04 +0800

    Generate blocked domain log, improve log readability. From roadrunnere42 http://itus.accessinnov.com/New-tab-in-web-filter-page-to-show-blocked-domains-td1284.html.

--- etc/itus/lists/log-gen.sh
+++ etc/itus/lists/log-gen.sh
@@ -1,3 +1,4 @@
+# Called by: web filter log window
 #set -x
 
 # Get network id
@@ -5,6 +6,9 @@ network_id_regex="inet [0-9]\+\.[0-9]\+\.[0-9]\+\."
 network_id=$(ip addr show br-lan | grep -o "${network_id_regex}" | head -1 | cut -d' ' -f2)
 blockdomain=`uci get network.blockdomain.ipaddr`
 logread | grep $network_id | awk '/query/ { print $1"  "$2"  "$3"  "$5"  "$4"  QUERIED:  "$9"  "$11}' > /tmp/wf-dirty.log
-logread | grep $blockdomain | awk ' /dnsmasq/ { print $1"  "$2"  "$3"  "$5"  "$4"  STOPPED:  "$9 }' >> /tmp/wf-dirty.log
+logread | grep $blockdomain | awk '/dnsmasq/ { print $1"  "$2"  "$3"  "$5"  "$4"  STOPPED:  "$9 }' >> /tmp/wf-dirty.log
+logread | grep $blockdomain | awk '/dnsmasq/ { print $1"  "$2"  "$3"  "$5"  "$4"  STOPPED:  "$9 }' > /tmp/dns_stopped_tmp.log
+sort -r /tmp/dns_stopped_tmp.log > /tmp/dns_stopped.log
+rm /dns_stopped_tmp.log
 sort -r /tmp/wf-dirty.log > /tmp/dns.log
 rm /tmp/wf-dirty.log
