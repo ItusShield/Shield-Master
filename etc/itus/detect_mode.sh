a2756c27bcc3fd1364a0f21e275db0e04aa1ddfe
Dave A-W <dave.aw+git@gmail.com>
2017-08-13 14:24:34 +0800

    Improve timings, output, comments

--- etc/itus/detect_mode.sh
+++ etc/itus/detect_mode.sh
@@ -1,6 +1,15 @@
 #!bin/sh
+#################################################################################
+# update_blacklist.sh						                                    #
+# By: Hans 									                                    #
+# version 2									                                    #
+# Modified: 14th March 2016							                            #
+# called by: /etc/rc.local                                                      #
+# writes to: file called .shield_mode in root   	                            #
+# Purpose: To determine which mode the Shield is running in.            	    #
+# changes: 									                                    #
+#################################################################################
 
-# Script by Roadrunner42
 
 MODE_FILE='/.shield_mode'
 SHIELD_MODE='Unknown'
@@ -17,7 +26,7 @@ fi
 # 	if  [ `df -h | grep -m1 mmcblk* | awk '{ print substr( $0, 6, 14 )  }'` ]; then
 		DISK_PARTITION=`df -h | grep -m1 mmcblk* | awk '{ print substr( $0, 6, 14 ) }'`
 # 	fi
-# Determnine shield mode
+# Determine shield mode
 	if   [ $DISK_PARTITION = 'mmcblk0p2' ]; then
 		SHIELD_MODE='Router'
 	elif [ $DISK_PARTITION = 'mmcblk0p3' ]; then
--- etc/itus/factory_reset.sh
+++ etc/itus/factory_reset.sh
@@ -1,29 +1,52 @@
+#############################################################################
+# Version 1                                                                 #
+# Allows a factory reset to occur from either gui or running this command   #
+# from the command line.                                                    #
+# Called by: from gui or command line                                       #
+#############################################################################
+
 if [[ `df | grep -c overlay` == "1" ]]; then
 	umount /overlay
 fi
-sleep 1        
+sleep 1
+     echo " Making new Directory"
+
 mkdir -p /factory_reset
-sleep 1                
+sleep 1
+      echo " mount factory_reset directory"
 mount /dev/mmcblk0p1 /factory_reset
-sleep 1                            
-cd /factory_reset                  
-sleep 1                            
+sleep 1
+
+cd /factory_reset
+sleep 1
+   echo "removing router image"
 rm ItusrouterImage
-sleep 5           
+sleep 5
+    echo "removing bridge image"
 rm ItusbridgeImage
-sleep 5           
+sleep 5
+    echo "removing gateway image"
 rm ItusgatewayImage
-sleep 5            
+sleep 5
+     echo "copying new router image"
 cp ItusrestoreImage ItusrouterImage
-sleep 10                           
+sleep 15
+     echo "copying new router image"
 cp ItusrestoreImage ItusgatewayImage
-sleep 10                            
-cp ItusrestoreImage ItusbridgeImage 
-sleep 10                            
-cd /                               
-sleep 1                            
+sleep 15
+     echo "copying new router image"
+cp ItusrestoreImage ItusbridgeImage
+sleep 15
+
+cd /
+echo " waiting 10 second, you may get messages that certain devices are busy"
+echo " and the display stops working,  a reboot has been started"
+echo " and the shield is updating itself, this can take upto 10 minutes, "
+echo " the screen will not be updated so go and have a cup of tea."
+echo " default password is itus"
+sleep 10
 umount /factory_reset
-sleep 1              
-umount -a            
-sleep 1              
+sleep 1
+umount -a
+sleep 1
 reboot -f
